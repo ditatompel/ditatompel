e782e26a581b24d00562f67f94943ed2cfa00f84
ditatompel <ditatompel@gmail.com>
2023-03-25 03:24:31 +0700

    Adding owl4ce's joygrayful rofi config files

---
+++ .config/rofi/scripts/modi-main.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Run rofi-main menu with built-in modi.
+# https://github.com/owl4ce/dotfiles
+
+# SPDX-License-Identifier: ISC
+
+SYSTEM_LANG="$LANG"
+export LANG='POSIX'
+exec >/dev/null 2>&1
+
+# Execute rofi-main menu window.
+LANG="$SYSTEM_LANG" \
+exec rofi -theme-str '@import "main.rasi"' \
+          -no-lazy-grab \
+          -modi run,combi,drun \
+          -show drun
+
+exit ${?}
