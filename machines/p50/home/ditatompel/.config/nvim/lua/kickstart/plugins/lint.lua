41378322d386d01c0e8058ed8d690b86a1afbc5c
Christian Ditaputratama <ditatompel@gmail.com>
2024-08-16 01:47:00 +0700

    Added `sh` linter using `shellcheck`
    Note that `shellcheck` need to be installed on the system.
    
    Installing `shellcheck` using `pacman` will install many haskel
    dependencies. Using `shellcheck-bin` from AUR or downloading
    pre-compiled binary can be alternative solution.

--- machines/p50/home/ditatompel/.config/nvim/lua/kickstart/plugins/lint.lua
+++ machines/p50/home/ditatompel/.config/nvim/lua/kickstart/plugins/lint.lua
@@ -6,7 +6,8 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
+        sh = { 'shellcheck' }, -- require shellcheck installed
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
