1fed53d3a6b484b8b6985ba7ff4c4218f78b9d6c
Christian Ditaputratama <ditatompel@gmail.com>
2024-08-21 04:38:34 +0700

    feat(nvim): Added lsp for c, rust, zig

--- machines/p50/home/ditatompel/.config/nvim/lua/lazy-plugins.lua
+++ machines/p50/home/ditatompel/.config/nvim/lua/lazy-plugins.lua
@@ -351,15 +351,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        --
+        --See `:help lspconfig-all` for a list of all the pre-configured LSPs
+        clangd = {},
+        rust_analyzer = {},
+        zls = {},
         prettierd = {
           filetypes = {
             'javascript',
@@ -662,6 +657,8 @@ require('lazy').setup({
       ensure_installed = {
         'bash',
         'c',
+        'rust',
+        'zig',
         'diff',
         'html',
         'lua',
