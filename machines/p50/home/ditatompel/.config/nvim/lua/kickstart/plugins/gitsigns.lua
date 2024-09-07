2282e42338ba9d2ba3aa4696289c37acc197e4b8
Christian Ditaputratama <ditatompel@gmail.com>
2024-09-07 18:11:45 +0700

    nvim: Modular gitsigns

--- machines/p50/home/ditatompel/.config/nvim/lua/kickstart/plugins/gitsigns.lua
+++ machines/p50/home/ditatompel/.config/nvim/lua/kickstart/plugins/gitsigns.lua
@@ -1,11 +1,15 @@
--- Adds git related signs to the gutter, as well as utilities for managing changes
--- NOTE: gitsigns is already included in init.lua but contains only the base
--- config. This will add also the recommended keymaps.
-
+-- See `:help gitsigns` to understand what the configuration keys do
 return {
-  {
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
@@ -59,3 +63,5 @@ return {
     },
   },
 }
+
+-- vim: ts=2 sts=2 sw=2 et
--- machines/p50/home/ditatompel/.config/nvim/lua/lazy-plugins.lua
+++ machines/p50/home/ditatompel/.config/nvim/lua/lazy-plugins.lua
@@ -20,24 +20,10 @@ require('lazy').setup({
   -- Use `opts = {}` to force a plugin to be loaded.
   --
 
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  -- modular approach: using `require 'path/name'` will
+  -- include a plugin definition from file lua/path/name.lua
 
+  require 'kickstart/plugins/gitsigns',
   require 'kickstart/plugins/which-key',
 
   -- NOTE: Plugins can specify dependencies.
