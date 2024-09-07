d7dd7d1380b195c980e7240360fb70dc2c186028
Christian Ditaputratama <ditatompel@gmail.com>
2024-09-07 19:08:52 +0700

    nvim: Added mode keymap

--- machines/p50/home/ditatompel/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ machines/p50/home/ditatompel/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -67,8 +67,9 @@ return {
           --
           -- In this case, we create a function that lets us more easily define mappings specific
           -- for LSP related items. It sets the mode, buffer and description for us each time.
-          local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
           end
 
           -- Jump to the definition of the word under your cursor.
