8ad3ab789a065b7595e96021e53a4f06fcd46883
Christian Ditaputratama <ditatompel@gmail.com>
2024-10-07 21:27:37 +0700

    feat: update references of tsserver to ts_ls (nvim-lua/kickstart.nvim#1131)

--- machines/p50/home/ditatompel/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ machines/p50/home/ditatompel/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -203,7 +203,7 @@ return {
         goimports = {},
         templ = { filetypes = { 'templ' } },
         htmx = { filetypes = { 'html' } },
-        tsserver = {},
+        ts_ls = {},
         svelte = { filetypes = { 'svelte' } },
         lua_ls = {
           -- cmd = {...},
@@ -243,7 +243,7 @@ return {
             local server = servers[server_name] or {}
             -- This handles overriding only values explicitly passed
             -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for tsserver)
+            -- certain features of an LSP (for example, turning off formatting for ts_ls)
             server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
             require('lspconfig')[server_name].setup(server)
           end,
